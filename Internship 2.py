d3bad824494719307b4ae825aaae0ff73c34be78
Abhishek-2704 <bhujbalabhishek2704@gmail.com>
2024-08-16 11:39:50 +0530

    Update Internship 2.py

--- Internship 2.py
+++ Internship 2.py
@@ -4,28 +4,19 @@ def count_words(text):
 
 def main():
     print("Welcome to the Word Counter Program!")
-    
     while True:
         try:
-           
             user_input = input("Please enter a sentence or paragraph (or type 'exit' to quit): ")
             
             if user_input.lower() == 'exit':
                 print("Thanks for using the Word Counter Program. Goodbye!")
                 break
-            
             if not user_input.strip():
                 print("Oops! It looks like you didn't enter anything. Please try again.")
                 continue
-            
             word_count = count_words(user_input)
-            
-         
             print(f"There are {word_count} words in the text you entered.")
-        
         except Exception as e:
-        
             print(f"An error occurred: {e}")
-
 if __name__ == "__main__":
     main()
